1e27ed82eb83905053810f554f754d510c6873bc
dep-official <github@dep.team>
2025-04-08 14:36:41 +0900

    init

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -27,13 +27,6 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   transpilePackages: ['zustand'],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, './src')
-    };
-    return config;
-  }
 };
 
 export default nextConfig; 
